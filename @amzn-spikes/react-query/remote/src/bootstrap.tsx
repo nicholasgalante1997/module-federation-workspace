5883dbfc133f14684466a359c72d3cd6d2a78f9d
Nick Galante <nicholasgalante1997@gmail.com>
2021-12-17 13:19:58 -0700

    changed bootstrap

---
+++ @amzn-spikes/react-query/remote/src/bootstrap.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const style = {
+    color: 'darkblue',
+    padding: '30px',
+    backgroundColor: 'deeppink'
+}
+
+ReactDOM.render(<div style={style}>Remote Query Consumer Package</div>, document.querySelector('#app'));
